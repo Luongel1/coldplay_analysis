8686fc7092f5bed6adbbfe6dec6e8afed38e8aad
Elizabeth Luong <elizabethh.luong@mail.utoronto.ca>
2024-11-21 14:55:01 -0500

    Updated spacing.

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -1,11 +1,11 @@
 ### Preamble ###
-# Purpose: Downloads and saves the data from spotify
+# Purpose: Downloads and saves the data from Spotify
 # Author: Abdullah Motasim, Elizabeth Luong, Yuanting Han
 # Date: 10 October 2024
 # Contact: abdullah.motasim@utoronto.ca
 # License: MIT
 # Pre-requisites: spotifyr
-# Any other information needed? Spotify developers account with client_id and client_secret
+# Any other information needed? Spotify developers account with `client_id` and `client_secret`
 
 
 #### Workspace setup ####
@@ -17,9 +17,6 @@ library(spotifyr)
 
 #### Download data ####
 coldplay <- get_artist_audio_features("coldplay")
-
-
-
 
 #### Save data ####
 saveRDS(coldplay, "data/raw_data.rds")
